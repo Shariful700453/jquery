d655478d097b04fa85ba38b7c941a8c93b358489
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2019-12-18 17:39:40 +0600

    slide done

---
+++ slider/main.js
@@ -0,0 +1,15 @@
+(function($) {
+	$('.slidedown').click(function(){
+		$('.slidedown1').slideDown();
+	});
+
+	$('.slideup').click(function(){
+		$('.slideup1').slideUp();
+	});
+
+	$('.slidetoggle').click(function(){
+		$('.slidetoggle1').slideToggle();
+	});
+
+
+})(jQuery);
