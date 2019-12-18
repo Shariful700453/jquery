1811faa09771d3d7a63f0b50f51223513c8c7030
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2019-12-18 18:26:06 +0600

    chaining done

---
+++ chaining/main.js
@@ -0,0 +1,14 @@
+(function($) {
+	$(".btn1").click(function(){
+		$(".p1").css({
+			"color":"red",
+			"font-size": "30px",
+			"padding": "50px",
+			"background": "black"
+		});
+	});
+
+	
+
+
+})(jQuery);
